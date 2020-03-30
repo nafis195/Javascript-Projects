238c24d0fd1d83e7f5853e4e74d15493ba3910ec
nafis195 <nafisbuet1@gmail.com>
2020-03-29 17:20:10 -0700

    final commit for project 1

--- Project 1 - Your Age in Days/script.js
+++ Project 1 - Your Age in Days/script.js
@@ -11,3 +11,17 @@ Time- 1:47 AM
 
 
 
+function ageInDays(){
+    var yourBirthYear = prompt("What is your birth year?");
+    var todayYear = prompt("What is today's year?");
+    var result = (todayYear - yourBirthYear) * 365;
+    var h1 = document.createElement("h1");
+    var answerAge = document.createTextNode("You are " + result + " days old.");
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(answerAge);
+    document.getElementById('answer').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('answer').remove();
+}
