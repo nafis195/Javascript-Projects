6ca07053500ac6cd308f164c153e03ee62bdef90
nafis195 <nafisbuet1@gmail.com>
2020-03-30 02:03:33 -0700

    final commit for project 2

--- Project 2 - Cat Generator/script.js
+++ Project 2 - Cat Generator/script.js
@@ -11,3 +11,9 @@ Time - 1:37 AM
 
 
 
+function catGenerate(){
+    var image = document.createElement('img');
+    var div = document.getElementById('show-cat');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
